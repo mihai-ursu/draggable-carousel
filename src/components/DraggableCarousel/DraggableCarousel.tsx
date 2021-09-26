eda9290b324dfce8831fda9a7a846de8735456cc
Mihai Ursu <mediabq@gmail.com>
2021-09-26 14:54:04 +0300

    changed the 1st version

--- src/components/DraggableCarousel/DraggableCarousel.tsx
+++ src/components/DraggableCarousel/DraggableCarousel.tsx
@@ -2,31 +2,43 @@ import React, { useRef } from "react";
 import styles from "./DraggableCarousel.module.scss";
 import { motion } from "framer-motion";
 import useElementSize from "../../hooks/useElementSize";
+import useWindowSize from "../../hooks/useWindowSize";
 
 const DraggableCarousel = () => {
+  const wrapperRef = useRef(null);
   const carouselRef = useRef(null);
 
+  const { width: wrapperWidth } = useElementSize(wrapperRef);
   const { width: carouselWidth } = useElementSize(carouselRef);
-  const dragConstrain = 1428 - carouselWidth;
+  const { width: windowWidth } = useWindowSize();
+
+  const dragConstrain = wrapperWidth - carouselWidth;
 
   return (
-    <motion.div
-      ref={carouselRef}
-      drag="x"
-      dragConstraints={{ left: dragConstrain, right: 0 }}
-      className={styles.carousel}
+    <div
+      ref={wrapperRef}
+      className={`${styles.carouselWrapper} ${
+        windowWidth <= 600 && styles.mobile
+      }`}
     >
-      <div className={styles.rectangle}></div>
-      <div className={styles.rectangle}></div>
-      <div className={styles.rectangle}></div>
-      <div className={styles.rectangle}></div>
-      <div className={styles.rectangle}></div>
-      <div className={styles.rectangle}></div>
-      <div className={styles.rectangle}></div>
-      <div className={styles.rectangle}></div>
-      <div className={styles.rectangle}></div>
-      <div className={styles.rectangle}></div>
-    </motion.div>
+      <motion.div
+        ref={carouselRef}
+        drag={windowWidth <= 700 ? "x" : false}
+        dragConstraints={{ left: dragConstrain, right: 0 }}
+        className={styles.carousel}
+      >
+        <div className={styles.rectangle}></div>
+        <div className={styles.rectangle}></div>
+        <div className={styles.rectangle}></div>
+        <div className={styles.rectangle}></div>
+        <div className={styles.rectangle}></div>
+        <div className={styles.rectangle}></div>
+        <div className={styles.rectangle}></div>
+        <div className={styles.rectangle}></div>
+        <div className={styles.rectangle}></div>
+        <div className={styles.rectangle}></div>
+      </motion.div>
+    </div>
   );
 };
 
